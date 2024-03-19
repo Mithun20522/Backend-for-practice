96c9fd43d7727b2d16f0aa60c6729c398a24bade
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-19 22:42:55 +0530

    get product api route done.

--- E-commerce/controllers/product.controller.js
+++ E-commerce/controllers/product.controller.js
@@ -26,3 +26,14 @@ export const getAllProducts = async(req, res) => {
         return res.status(500).json({error:error.message});
     }
 }
+
+export const getProduct = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const isExist = await Product.findById({_id:id});
+        if(!isExist) return res.status(404).json({message: 'Product not found'});
+        return res.status(200).json(isExist);
+    } catch (error) {
+        return res.status(500).json({error:error.message});
+    }
+}
--- E-commerce/routes/product.route.js
+++ E-commerce/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { create, getAllProducts } from '../controllers/product.controller.js';
+import { create, getAllProducts, getProduct } from '../controllers/product.controller.js';
 
 const productRouter = express.Router();
 
 productRouter.post('/create',create);
 productRouter.get('/getproducts',getAllProducts);
+productRouter.get('/getproduct/:id',getProduct);
 
 export default productRouter;
