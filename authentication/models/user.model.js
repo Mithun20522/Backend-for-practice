ce879b9bf3c53a15bb907f0a8e6bebca7d5f82b9
mithun20522 <mithun20522@iiitd.ac.in>
2024-03-19 16:52:47 +0530

    password hashed

--- authentication/models/user.model.js
+++ authentication/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from 'bcryptjs';
 const userSchema = new mongoose.Schema({
     username:{
         type:String,
@@ -16,6 +16,19 @@ const userSchema = new mongoose.Schema({
     }
 },{timestamps:true});
 
+userSchema.pre('save', async function(next){
+    const user = this;
+    if(!user.isModified('password')) return next();
+    try {
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(user.password, salt);
+        this.password = hashedPassword;
+        next();
+    } catch (error) {
+        console.log(error)
+    }
+
+})
 
 const User = mongoose.model('User', userSchema);
 
